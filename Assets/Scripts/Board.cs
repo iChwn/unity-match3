5f639d6a98fb0bba11466219c21721f9304d7b47
ichwn <ichwanarif26@gmail.com>
2022-10-31 22:24:39 +0700

    Initialize project

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Board : MonoBehaviour
+{
+
+    public static Board Instance { get; private set; }
+
+    public Row[] rows;
+
+    public Tile[] Tiles { get; private set; }
+
+    public int Width => Tiles.GetLength(0);
+    public int Height => Tiles.GetLength(1);
+
+    private void Awake() => Instance = this;
+
+    private void Start()
+    {
+        // print(Tiles);
+        // Tiles  = new Tile[rows.Max(row => row.tiles.Length), rows.Length];
+
+        // for (int y = 0; y < Height; y++)
+        // {
+        //     for (int x = 0; x < Width; x++)
+        //     {
+        //         var tile = rows[y].tiles[x];
+
+        //         tile.x = x;
+        //         tile.y = y;
+
+        //         tile.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
+
+        //         Tiles[x,y] = tile;
+        //     }
+        // }        
+    }
+}
